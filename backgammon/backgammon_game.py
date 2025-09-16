4d1e75cca9da96f7164894297a3623fc69244a4d
Santiago García <santiagotgarcia464@gmail.com>
2025-09-15 23:47:23 -0300

    Extender clase Game con turnos y tiradas de dados

--- backgammon/backgammon_game.py
+++ backgammon/backgammon_game.py
@@ -1,5 +1,49 @@
-class BackgammonGame:
+"""
+Módulo backgammon_game
+Coordina la lógica principal del juego de Backgammon.
+"""
 
-    def __init__(self):
-        self.__tablero__ = None
-        self.__jugadores__ = None
+from .player import Player
+from .board import Board
+from .dice import Dice
+
+
+class BackgammonGame:
+    """
+    Coordina el flujo del juego.
+    Maneja jugadores, tablero, dados y turnos.
+    """
+
+    def __init__(self, player1_name: str = "Jugador 1", player2_name: str = "Jugador 2"):
+        self.__tablero__ = Board()
+        self.__dados__ = Dice()
+        self.__jugadores__ = [Player(player1_name), Player(player2_name)]
+        self.__turno_actual__ = 0  # índice del jugador actual
+        self.__tablero__.setup_board(self.__jugadores__)
+
+    @property
+    def tablero(self) -> Board:
+        """Devuelve el tablero del juego."""
+        return self.__tablero__
+
+    @property
+    def jugadores(self) -> list:
+        """Devuelve la lista de jugadores."""
+        return self.__jugadores__
+
+    @property
+    def jugador_actual(self) -> Player:
+        """Devuelve el jugador que tiene el turno."""
+        return self.__jugadores__[self.__turno_actual__]
+
+    def tirar_dados(self):
+        """
+        Tira los dados y retorna el resultado.
+        """
+        return self.__dados__.roll()
+
+    def cambiar_turno(self):
+        """
+        Cambia el turno al otro jugador.
+        """
+        self.__turno_actual__ = 1 - self.__turno_actual__
